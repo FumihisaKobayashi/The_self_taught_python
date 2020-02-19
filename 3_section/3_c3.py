69e2edafa1f6411d768261f6ebce28c6b15310c8
FumihisaKobayashi <ruruku717@gmial.com>
2020-02-19 19:45:38 +0900

    チャレンジ

---
+++ 3_section/3_c3.py
@@ -0,0 +1,10 @@
+a = 5
+
+if a <= 10:
+    print("10以下")
+
+elif a > 10 and a <= 25:
+    print("25以下")
+
+elif a > 25:
+    print("25より大きい")
---
+++ 3_section/3_c5.py
@@ -0,0 +1,14 @@
+age = 27
+
+if age <= 12:
+    print("小学生の年齢")
+
+elif age <= 15:
+    print("中学生の年齢")
+
+elif age <= 18:
+    print("高校生の年齢")
+
+else:
+    print("大人と判断してよろしいでしょう！")
+
