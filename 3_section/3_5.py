79570843303e291af6270ff7387c4204af468bed
FumihisaKobayashi <ruruku717@gmial.com>
2020-02-19 18:00:55 +0900

    変数と定数

---
+++ 3_section/3_5.py
@@ -0,0 +1,8 @@
+x = 10
+y = 10
+
+z = x + y
+a = x - y
+
+print(z)
+print(a)
---
+++ 3_section/3_7.py
@@ -0,0 +1,10 @@
+
+hi = "HELLO WORLD"
+
+#スペースできないので_の入力を開ける際には
+my_floot = 2.2
+
+my_boolean = "true"
+
+#例
+print(my_boolean)
