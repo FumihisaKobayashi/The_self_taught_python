949bc39d8043d86cc373f4a769661e76f57c5363
FumihisaKobayashi <ruruku717@gmial.com>
2020-02-19 20:33:36 +0900

    関数その２

---
+++ 4_section/4_2.py
@@ -0,0 +1,17 @@
+#数値が当てはまらない際はそのまま。
+
+def f():
+    return 1+1
+
+result = f()
+print(result)
+
+
+#複数囲う場合は、(a,b,c)との表記にする。
+def f(x, y, z):
+    return x + y + z
+
+result = f(1,2,3)
+
+print(result)
+
