e3f8bffc3e9b5d5664b881e9db00b94be7ae243f
FumihisaKobayashi <ruruku717@gmial.com>
2020-02-19 21:08:51 +0900

    関数の利用２

---
+++ 4_section/4_4.py
@@ -0,0 +1,14 @@
+#関数の再利用をしたケース。
+
+def even_odd():
+    n = input("type a number!")
+    b = int(n)
+    if n % 2== 0:
+        print("n is even")
+    else:
+        print("n is odd")
+
+
+even_odd()
+even_odd()
+even_odd()
