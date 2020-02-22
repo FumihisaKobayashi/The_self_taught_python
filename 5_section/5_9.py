cde4d6795fb84e02efdf576a90822fae8210f867
FumihisaKobayashi <ruruku717@gmial.com>
2020-02-22 22:29:27 +0900

    タプル（オブジェクト保存）

---
+++ 5_section/5_9.py
@@ -0,0 +1,9 @@
+#タプルは変更不可能.リストは[].タプルは()区別
+my_tuple = ("MARIO")
+print(my_tuple)
+#複数ある場合は、()内,で区切る。
+rndm = ("M. Jaction", 1958, True)
+print(rndm)
+#要素取り出しはインデックスでもできる。[数字]なので注意。
+
+print(rndm[1])
