34c0d6558a492867a72c941b8646e84c9907872b
FumihisaKobayashi <ruruku717@gmial.com>
2020-02-22 21:53:13 +0900

    リストに何か含まれているか調べる

---
+++ 5_section/5_7.py
@@ -0,0 +1,8 @@
+colors = ["blue", "green", "yellow"]
+#リストに要素が含まれているのを調べるのは in を使う。
+print("green" in colors)
+#入ってない事を確認する時は not in
+print("black" not in colors)
+#コンテナの中の要素が何個あるかは len を使う。（リストの個数）
+print(len(colors))
+
