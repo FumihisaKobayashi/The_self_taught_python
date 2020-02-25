4ca01c0d1fdcc13425245963d0c401ed9c67f0b3
FumihisaKobayashi <ruruku717@gmial.com>
2020-02-26 01:19:51 +0900

    チャレンジ４

---
+++ 5_section/5_c4.py
@@ -0,0 +1,13 @@
+fumi = {
+    "身長": "1.73m",
+    "好きな色": "緑",
+    "好きな人": "Hideki Matsui"
+}
+
+answer = input("身長,好きな色 or 好きな人")
+if answer in fumi:
+    a = fumi[answer]
+    print(a)
+
+
+#:注意
