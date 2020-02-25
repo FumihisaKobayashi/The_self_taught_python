8f1062965f8717b4a8194e6828c9a1ef6d852599
FumihisaKobayashi <ruruku717@gmial.com>
2020-02-26 00:55:44 +0900

    チャレンジ１

---
+++ 5_section/5_c1.py
@@ -0,0 +1,10 @@
+lists = []
+
+favo = ["KEYTALK", "BLUE ENCOUNT", "SCANDAL", "Aimer"]
+been = ["UVERworld", "Aqua Timez", "SPLAY", "BAMP"]
+
+lists.append(favo)
+lists.append(been)
+
+print(lists)
+
