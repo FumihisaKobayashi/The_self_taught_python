a7978ce89799eac266a8e011bc8178df860766c4
FumihisaKobayashi <ruruku717@gmial.com>
2020-02-25 23:22:35 +0900

    #[辞書名] [キー] = [バリュー]

---
+++ 5_section/5_12.py
@@ -0,0 +1,18 @@
+#[辞書名] [キー] = [バリュー]
+
+facts = dict()
+
+#バリューを追加
+facts["code"] = "fun"
+#キーで参照
+print(facts["code"])
+
+#バリューを追加
+facts["Bill"] = "Gates"
+#キーで参照
+print(facts["Bill"])
+#バリューを追加
+facts["founded"] = 1776
+#キーで参照
+print(facts["founded"])
+
