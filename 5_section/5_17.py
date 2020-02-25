8199b05ebff394738f9e0e9a2a9ebf4b867ef4aa
FumihisaKobayashi <ruruku717@gmial.com>
2020-02-26 00:39:41 +0900

    コンテナ(リスト,タプル,辞書)

---
+++ 5_section/5_17.py
@@ -0,0 +1,29 @@
+
+#リスト
+locations = []
+
+la = (44.1, 118)
+chi = (55.8, 998)
+
+locations.append(la)
+locations.append(chi)
+
+print(locations)
+
+#タプル
+egg = ["potate", "tomato"]
+nin = ["Hemi", "Fit", "Vezel"]
+aio = (egg, nin)
+
+print(aio)
+
+#辞書
+bday = {"deku": "7996.65",
+        "allmate": "1229.4.6"}
+
+my_lists = bday
+print(my_lists)
+my_taple = (bday,)
+print(my_taple)
+
+
