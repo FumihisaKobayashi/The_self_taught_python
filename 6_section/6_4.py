8e68e6d3a6edf58fb413d4b94b7fb5cec082663b
FumihisaKobayashi <ruruku717@gmial.com>
2020-02-27 23:30:18 +0900

    .upper() .lower() .capitalize()大文字小文字

---
+++ 6_section/6_4.py
@@ -0,0 +1,12 @@
+
+#.upper()にて全て大文字
+a = "We are the world...".upper()
+print(a)
+
+#.lower()にて全て小文字
+b = "SO IT GOES".lower()
+print(b)
+
+#.capitalize()にて1文字目大文字。
+c = "four score and...".capitalize()
+print(c)
