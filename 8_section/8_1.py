8cd67d0508d481e0b42423bfaaa29379a7e108e7
FumihisaKobayashi <ruruku717@gmial.com>
2020-03-08 23:32:51 +0900

    モジュール#import.[]

---
+++ 8_section/8_1.py
@@ -0,0 +1,16 @@
+
+#import [モジュール名]
+import math
+
+
+#モジュールをインポートしたら、[モジュール名].[コード]
+print(math.pow(2, 3))
+#.powは左を右回乗数する。
+
+
+import random
+
+#結果はランダムだから変わる
+print(random.randint(0, 100))
+
+#.randint関数は、整数の乱数を発生させる。
