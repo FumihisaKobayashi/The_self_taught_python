52fcf6ff007ec62729093fa98589cd1563331d00
FumihisaKobayashi <ruruku717@gmial.com>
2020-03-08 23:38:02 +0900

    モジュール[statistics]

---
+++ 8_section/8_2.py
@@ -0,0 +1,14 @@
+
+#statisticsは平均、中央、最頻を計算して返す。
+import statistics
+
+#mean(平均値)
+nums = [1, 5, 33, 12, 46, 33, 2]
+print(statistics.mean(nums))
+
+#median(中央値)
+print(statistics.median(nums))
+
+#mode(最頻値)
+print(statistics.mode(nums))
+
