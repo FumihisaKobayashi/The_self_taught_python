432eb84b0f032e4a427eff350bc274bd52020d16
FumihisaKobayashi <ruruku717@gmial.com>
2020-03-15 23:59:40 +0900

    チャレンジ２

--- 10_section/10_2.py
+++ 10_section/10_2.py
@@ -1,14 +1,16 @@
 
-
+#stagesを１マイナスはstagesは０からだが、wrongは１からのため
 while wrong < len(stages) - 1:
     print("\n")
     msg = "一文字を予想してね"
     #input使って変数charに割り当て
     char = input(msg)
+    #input使って入力された数値をcharに割り当て
     if char in rletters:
         cind = rletters.index(char)
         board[cind] = char
         rletters[cind] = '$'
+        #複数同じ文字あると正しく動作しないので、正解したものを＄に書き換え
     else:
         wrong += 1
 
