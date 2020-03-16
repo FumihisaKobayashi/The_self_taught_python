fb2d658d4f11b30678f94ed0615f1fee1acc365b
FumihisaKobayashi <ruruku717@gmial.com>
2020-03-16 23:20:39 +0900

    クラスの例(オレンジ)

---
+++ 12_section/12_4.py
@@ -0,0 +1,8 @@
+class Orange:
+    def __init__(self, w, c):
+        self.weight = w
+        self.color = c
+        print("Created!")
+
+#__init__など二重のアンダースコア　ー特殊メソッド
+#Orangeオブジェクトh関数を呼び出すのと同じ構文[クラス名](引数)
