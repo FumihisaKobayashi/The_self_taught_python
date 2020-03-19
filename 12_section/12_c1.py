57957380b379099b14ec1756d3d21a778b94ef58
FumihisaKobayashi <ruruku717@gmial.com>
2020-03-19 23:20:59 +0900

    チャレンジ１

---
+++ 12_section/12_c1.py
@@ -0,0 +1,10 @@
+class Apple:
+    def __init__(self, w, c, s, t):
+        self.wight = w
+        self.color = c
+        self.size = s
+        self.tree = t
+        print("Created!")
+
+
+ap1 =Apple(100, "red", "circle", "tree")
