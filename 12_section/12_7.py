7fa6dca6b135fcb150ed14cd0b4e76dcbed30791
FumihisaKobayashi <ruruku717@gmial.com>
2020-03-19 22:45:30 +0900

    インスタンス変数の値変更#[オブジェクト名].[インスタンス変数] = [新しい値]

--- 12_section/12_7.py
+++ 12_section/12_7.py
@@ -1,12 +1,13 @@
+#インスタンス変数の値を変更する。
 
-#[オブジェクト名].[インスタンス変数] = [新しい値]
+
 class Orange:
     def __init__(self, w, c):
         self.weight = w
         self.color = c
         print("Created!")
 
-
+#[オブジェクト名].[インスタンス変数] = [新しい値]
 or1 = Orange(10, "dark orange")
 or1.weight = 100
 or1.color = "light orange"
