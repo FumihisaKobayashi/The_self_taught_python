eb1c741169ee353d7af4eb1f7dedd7e027b604f0
FumihisaKobayashi <ruruku717@gmial.com>
2020-03-20 22:17:24 +0900

    承継(親)

---
+++ 13_section/13_5.py
@@ -0,0 +1,10 @@
+class Shape:
+    def __init__(self, w, l):
+        self.width = w
+        self.len = l
+    
+    def print_size(self):
+        print("{} by {}".format(self.width, self.len))
+
+my_shape = Shape(20, 25)
+my_shape.print_size()
