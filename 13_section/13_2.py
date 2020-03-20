d120a1c600071fa59d8ccfcd55a19e786b5a1306
FumihisaKobayashi <ruruku717@gmial.com>
2020-03-20 21:10:58 +0900

    client(クラスの外側からオブジェクトを利用するコード

---
+++ 13_section/13_2.py
@@ -0,0 +1,10 @@
+#カプセル化　１。オブジェクトによって複数の変数２。メソッドをまとめる事
+
+class Data:
+    def __init__(self):
+        self.nums[index] = [1, 2, 3, 4, 5]
+    
+    def change_data(self, index, n):
+        self.nums[index] = n
+
+#クラス内にデータを隠蔽して外から見えないようにする。
