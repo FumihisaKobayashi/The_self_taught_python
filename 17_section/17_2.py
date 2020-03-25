2de79b8610f6e97192470ee39fffbea585a5f4f2
FumihisaKobayashi <ruruku717@gmial.com>
2020-03-26 01:30:36 +0900

    IGNORECASE

--- 17_section/17_2.py
+++ 17_section/17_2.py
@@ -3,5 +3,5 @@ import re
 l = "Beautiful is better than ungly."
 matches = re.findall("Beautiful", l, re.IGNORECASE)
 
-#大文字小文字の違いを無視して検索したい場合はfindall関数めの３つ目にre.IGNORECASEフラグ
+#大文字小文字の違いを無視して検索したい場合はfindall関数めの３つ目にre.IGNORECASEフラグを渡す
 print(matches)
