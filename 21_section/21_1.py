f3dba238837372bf31531c69fd445746b74652e4
FumihisaKobayashi <ruruku717@gmial.com>
2020-03-29 21:28:34 +0900

    スタック

--- 21_section/21_1.py
+++ 21_section/21_1.py
@@ -5,16 +5,16 @@
 class Stack:
     def __init__(self):
         self.items = []
-    
+    #スタックが空のときTRUEを返す中に要素がある時ファlsえを返す。
     def is_empty(self):
         return self.items == []
-
+    #要素を削除して返す。
     def push(self, item):
         self.items.append(item)
-    
+    #スタックの一番上を返すが削除しない。
     def pop(self):
         return self.items.pop()
-
+    #スタックの中の要素数を整数で返す。
     def peek(self):
         last = len(self.tems) - 1
         return self.items[last]
