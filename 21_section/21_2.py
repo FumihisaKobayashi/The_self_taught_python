8a85871f5d5176a970070587503a566723a956fe
FumihisaKobayashi <ruruku717@gmial.com>
2020-03-29 21:31:22 +0900

    スタック２TRUE(空)

---
+++ 21_section/21_2.py
@@ -0,0 +1,24 @@
+
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        last = len(self.tems) - 1
+        return self.items[last]
+
+    def size(self):
+        return len(self.items)
+
+#最初からにしてるのでTRUEを刺す
+stack = Stack()
+print(stack.is_empty())
