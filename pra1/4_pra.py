ddbc4080e2c1dbec8eec5cee60a8d6987b342d69
FumihisaKobayashi <ruruku717@gmial.com>
2020-06-24 21:08:30 +0900

    因数練習

---
+++ pra1/4_pra.py
@@ -0,0 +1,22 @@
+class Rectangle:
+    def __init__(self, w, l):
+        self.width = w
+        self.len = l
+
+def area(self):
+    return self.width * self.len
+
+#lenとwidthがインスタンス変数
+
+
+#[クラス名]([引数])
+class Orange:
+    def __init__(self, w, c):
+        self.weight = w
+        self.color = c
+        print("Created!")
+
+or1 = Orange(10, "dark orange")
+print(or1)
+
+#クラスを定義してからOrange[class](10, "dark orange")のコードでインスタンス化
