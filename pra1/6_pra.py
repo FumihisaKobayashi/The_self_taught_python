198c5a3fc6c37b48f713b95d1ba3d23ae3f98d54
FumihisaKobayashi <ruruku717@gmial.com>
2020-06-26 22:11:24 +0900

    リスト

--- pra1/6_pra.py
+++ pra1/6_pra.py
@@ -14,3 +14,12 @@ random.append("Hello")
 
 print(random)
 #いろんな物ができる。
+
+
+colors = ["blue", "green", "yellow"]
+colors
+#何も指定しなかったら、一番後ろが削除される。
+item = colors.pop()
+#.pop()の中インデックス指定すると、指定番号削除
+item
+print(colors)
