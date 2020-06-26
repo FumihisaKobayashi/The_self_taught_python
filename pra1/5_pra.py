6868a5c8a1c21c860673bec3f5ed270a73263332
FumihisaKobayashi <ruruku717@gmial.com>
2020-06-26 21:58:51 +0900

    Forループ

---
+++ pra1/5_pra.py
@@ -0,0 +1,15 @@
+#1文字ずつ取り出し。
+name = "Ted"
+for character in name:
+    print(character)
+
+#リスト要素をfor ループで取り出す。
+shows = ["GOT", "Narcos", "Vice"]
+for show in shows:
+    print(show)
+
+#タプルを　for で繰り返し。
+coms = ("A.Development", "Friends", "Always Sunny")
+for syou in coms:
+    print(syou)
+
