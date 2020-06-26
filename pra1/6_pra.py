d37057e4c227f479635f787b832517fe311f635e
FumihisaKobayashi <ruruku717@gmial.com>
2020-06-26 22:10:15 +0900

    リスト復習

---
+++ pra1/6_pra.py
@@ -0,0 +1,16 @@
+#新しい要素を追加
+fruit = ["Apple", "Orange", "Pear"]
+fruit.append("Banana")
+fruit.append("Peach")
+
+print(fruit)
+
+#appendで新しい要素追加
+random = []
+random.append(True)
+random.append(100)
+random.append(1.1)
+random.append("Hello")
+
+print(random)
+#いろんな物ができる。
