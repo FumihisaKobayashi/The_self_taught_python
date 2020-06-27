ff931d3e0d0d1263990e5757269efa84361e3c5a
FumihisaKobayashi <ruruku717@gmial.com>
2020-06-27 20:01:24 +0900

    ファイル

---
+++ pra1/7_pra.py
@@ -0,0 +1,35 @@
+import os
+print(os.path.join("Users", "bob", "st.txt"))
+
+#ファイルインポート
+
+#ファイル書き込み
+
+#モード
+# "r"はファイル読み取り専用
+# "w"はファイル書き出し専用
+# "w+"は、読み書き両方できるようにファイルを開く
+
+
+#open関数使ってファイル開いた後、open関数の戻り値をstに代入して書き込み。最後にcloseでファイル閉じる。
+
+#writeメソッドを使ってファイル書き込み
+st = open("st.txt", "w")
+st.write("HI from Python!")
+st.close()
+
+
+#read メソッドで呼び出し
+
+
+with open("st.txt", "r") as f:
+    print(f.read())
+
+
+#ファイル閉じる
+
+
+with open("st.txt", "w") as f:
+    f.write("Hi from Python")
+
+#code終了後ファイルを閉じる。
