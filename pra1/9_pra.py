92876990292cbcca24100639bde04b3e87f1e090
FumihisaKobayashi <ruruku717@gmial.com>
2020-06-29 23:28:33 +0900

    スクレイピング

---
+++ pra1/9_pra.py
@@ -0,0 +1,12 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+class Scraper:
+    def __init__(self, site):
+        self.site = site
+
+    def scrape(self):
+        pass
+#__init__メソッドはスクレイピング対象になるWEBのURLを受け取る。
+
