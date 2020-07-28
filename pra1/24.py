329d376689029b75da73a6f98715cc7e83e8cc2c
FumihisaKobayashi <ruruku717@gmial.com>
2020-07-28 23:39:24 +0900

    確認3if 範囲指定

---
+++ pra1/24.py
@@ -0,0 +1,11 @@
+def if_test2(num):
+    if 50 < num < 100:
+        print('50 < num < 100')
+    else:
+        print('num <= 50 or num >= 100')
+
+if_test2(70)
+# 50 < num < 100
+
+if_test2(0)
+# num <= 50 or num >= 100
